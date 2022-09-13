ede70a5e8bf253c791538c3c09b4e6150ae29b42
Leo Fuchs <leonardo.m.fuchs@gmail.com>
2022-09-12 18:51:35 -0600

    list model routes and controller

---
+++ app/controllers/api/lists_controller.rb
@@ -0,0 +1,43 @@
+class Api::ListsController < ApplicationController
+  before_action :set_list, only: [:show, :update, :destroy]
+
+  # model name -> List
+  def index
+    render json: List.all 
+  end
+
+  def show
+    render json: @list
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save 
+      render json: @list
+    else
+      render json: { errors: @list.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @list.update(list_params)
+      render json: @list
+    else
+      render json: { errors: @list.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list.destroy
+    render json: { message: 'List deleted' }
+  end
+
+  private 
+    def list_params
+      params.require(:list).permit(:title, :desc)
+    end
+
+    def set_list
+      @list = List.find(params[:id])
+    end
+end
